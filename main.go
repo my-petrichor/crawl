7d9fad4aa9a265e96f35e85f3b04d0549edd7f1b
my-Sakura <69021934+my-Sakura@users.noreply.github.com>
2020-12-15 17:03:12 +0800

    a simple crawl

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"regexp"
 	"strconv"
+	"sync"
+	"time"
 
 	"github.com/gocolly/colly"
 )
@@ -14,31 +17,46 @@ const (
 )
 
 var (
-	departments = make(map[string]int)
-	names       = make([]string, 0)
-	resumes     = make([]string, 0)
+	wg = sync.WaitGroup{}
 )
+
+type people struct {
+	departments              []string
+	department_people_number []int
+	names                    []string
+	resumes                  []string
+}
 
 func main() {
 	c := colly.NewCollector()
 	c.AllowURLRevisit = true
 	c.DetectCharset = true
 
+	var p people
+
+	wg.Add(1)
+
 	//The purpose of the reg is to get department_people_number
 	reg := regexp.MustCompile("[1-9]+")
 
+	u := time.Now()
 	c.OnHTML("dl#allNameBar", func(e *colly.HTMLElement) {
-		e.ForEach("dt", func(_ int, element *colly.HTMLElement) {
-			departmentName := element.Text
-			department_people_number, _ := strconv.Atoi(reg.FindString(element.ChildText("em")))
-			departments[departmentName] = department_people_number
-		})
+		go func() {
+			e.ForEach("dt", func(_ int, element *colly.HTMLElement) {
+				p.departments = append(p.departments, element.Text)
+				department_people_number, _ := strconv.Atoi(reg.FindString(element.ChildText("em")))
+				p.department_people_number = append(p.department_people_number, department_people_number)
+			})
+			wg.Done()
+		}()
 
 		e.ForEach("a[href]", func(_ int, ele *colly.HTMLElement) {
 			name := ele.Text
-			names = append(names, name)
+			p.names = append(p.names, name)
 
 			link := ele.Attr("href")
+
+			fmt.Println("visiting =>", link)
 
 			c.Visit(e.Request.AbsoluteURL(link))
 		})
@@ -46,26 +64,30 @@ func main() {
 
 	c.OnHTML("div.contentBar", func(e *colly.HTMLElement) {
 		if e.DOM.Find("p:contains(院士)").Text() != "" {
-			resumes = append(resumes, e.DOM.Find("p:contains(院士)").Text())
+			p.resumes = append(p.resumes, e.DOM.Find("p:contains(院士)").Text())
 		} else {
-			resumes = append(resumes, e.DOM.Find("font:contains(院士)").Text())
+			p.resumes = append(p.resumes, e.DOM.Find("font:contains(院士)").Text())
 		}
 	})
 
 	c.Visit(url)
+	wg.Wait()
+	t := time.Now()
+	crawlDuration := t.Sub(u)
+	fmt.Println(crawlDuration)
 
 	file, _ := os.Create(filename)
 	defer file.Close()
 
 	sum := 0
 	max := 0
-	for dep, number := range departments {
+	for i, dep := range p.departments {
 		file.WriteString(dep + "\n")
 
-		max += number
+		max += p.department_people_number[i]
 
 		for sum < max {
-			file.WriteString(names[sum] + ":" + " " + resumes[sum] + "\n")
+			file.WriteString(p.names[sum] + ":" + " " + p.resumes[sum] + "\n")
 			sum++
 		}
 
