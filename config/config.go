7f6fb97ae08a5a0a113de6ab6fcd89da7d2c5a33
my-Sakura <69021934+my-Sakura@users.noreply.github.com>
2020-12-29 01:13:17 +0800

    add config

---
+++ config/config.go
@@ -0,0 +1,40 @@
+package config
+
+import (
+	"fmt"
+
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	StateDepartmentNews     string `mapstructure: "stateDepartmentNews"`
+	StateDepartmentPolicies string `mapstructure: "stateDepartmentPolicies`
+	Economist               string `mapstructure: "economist`
+	Academician             string `mapstructure: "academician`
+}
+
+var Conf = new(Config)
+
+func Set() {
+	viper.SetConfigFile("config/conf/test.yaml")
+
+	if err := viper.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			fmt.Printf("config file could found, other error: %v\n", err)
+		} else {
+			fmt.Printf("config file not found: %v\n", err)
+		}
+	}
+
+	if err := viper.Unmarshal(Conf); err != nil {
+		panic(fmt.Sprintf("unmarshal error %v\n", err))
+	}
+
+	// viper.WatchConfig()
+
+	// viper.OnConfigChange(func(in fsnotify.Event) {
+	// 	if err := viper.Unmarshal(Conf); err != nil {
+	// 		panic(fmt.Sprintf("unmarshal error %v\n", err))
+	// 	}
+	// })
+}
